31108222f56c9c5ba29d2a2ae9a3a035d16c702e
Cassio Silva <cassioefs@dcc.ufrj.br>
2025-03-25 22:12:48 -0300

    correção branch inicial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Digite seu nome:");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string nome = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+Console.WriteLine("Bem vindo {0} {1}", nome, "Bla blaa bla");
